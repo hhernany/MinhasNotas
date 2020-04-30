242b963ac9b027bf42fdc9f98082c13fc2785d47
Hugo Hernany <hugohernany@Hugos-iMac-3.local>
2020-04-30 09:23:32 -0700

    First commit

---
+++ MinhasPautas/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MinhasPautas
+//
+//  Created by Hugo Hernany on 30/04/20.
+//  Copyright © 2020 Hugo Hernany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
